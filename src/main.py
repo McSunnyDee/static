7c399ec338051be716d24c0fb8c020a2cbc8eacd
McSunnyDee <dmingshz@gmail.com>
2025-09-24 15:14:02 -0300

    why doesn't it work

--- src/main.py
+++ src/main.py
@@ -1,32 +1,45 @@
-import re
-from textnode import TextNode, TextType
-from text2html import extract_markdown_images, extract_markdown_links, text_to_textnodes, block_to_block_type, markdown_to_html_node
+import os
+import shutil
+
+
+def clone_directory(source, destination):
+    print(source)
+    print(destination)
+    if os.path.exists(source) and not os.path.isfile(source):
+        for file_or_folder in os.listdir(source):
+            if os.path.isfile(file_or_folder):
+                shutil.copy(os.path.join(source, file_or_folder), os.path.join(destination, file_or_folder))
+            elif not os.path.isfile(file_or_folder):
+                #os.mkdir(os.path.join(destination, file_or_folder))
+                pass
+'''
+    #if a path exists
+    if os.path.exists(source):
+        #is file
+        if os.path.isfile(source) and not os.path.exists(destination):
+            shutil.copy(source, destination)
+        #is directory
+        else:
+            #check the directory for more files and folders...
+            print(os.listdir(source))
+            for file_or_folder in os.listdir(source):
+                source_file_or_folder = os.path.join(source, file_or_folder)
+                destination_file_or_folder = os.path.join(destination, file_or_folder)
+                if not os.path.isfile(file_or_folder):
+                    return clone_directory(source_file_or_folder, destination_file_or_folder)
+                shutil.copy(source_file_or_folder, destination_file_or_folder)
+'''
+
 
 def main():
-    #dummy = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    #print(dummy)
-    #nodes = text_to_textnodes(r"This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)")
-    #print(nodes)
-    #block = ">one\n>two\nthree"
-    #print(list((r"^\>.+", line) for line in block.split("\n")))
-    #print(
-    #    list(
-    #        filter(
-    #            lambda x: re.findall(r"^\>.*", x), (line for line in block.split("\n"))
-    #        )
-    #    )
-    #)
-    #print(block_to_block_type(block))
-    md = """
-```
-This is text that _should_ remain
-the **same** even with inline stuff
-```
-"""
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+        os.mkdir("public")
+    else: os.mkdir("public")
 
-    node = markdown_to_html_node(md)
-    html = node.to_html()
-    print(html)
+    #print(os.listdir("static"))
+    clone_directory("static", "public")
+
 
 
 main()
